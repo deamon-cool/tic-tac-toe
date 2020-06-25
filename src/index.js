eb8168c5f761312f29140a6769b695ac47f43d22
daemon-cool <damian.damian.tofik@gmail.com>
2020-06-25 18:36:40 +0200

    added square componenet

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+
+let root = document.querySelector('#root');
+
+class Square extends React.Component {
+    render() {
+        return (
+            <button className='square'>
+
+            </button>
+        );
+    }
+}

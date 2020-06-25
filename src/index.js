01458784d8fd44d50d5406bf119e71577c2cd36f
daemon-cool <damian.damian.tofik@gmail.com>
2020-06-25 18:44:50 +0200

    created game componenet

--- src/index.js
+++ src/index.js
@@ -44,3 +44,19 @@ class Board extends React.Component {
         );
     }
 }
+
+class Game extends React.Component {
+    render() {
+        return (
+            <div className='game'>
+                <div className='game-board'>
+                    <Board />
+                </div>
+                <div className='game-info'>
+                    <div>{}</div>
+                    <div>{}</div>
+                </div>
+            </div>
+        );
+    }
+}

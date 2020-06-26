00ea2ad004185e2b99136c8367590e22202a356d
daemon-cool <damian.damian.tofik@gmail.com>
2020-06-26 11:04:01 +0200

    added conditional that interrupt the game if someone won or if some player wants to check filled square

--- src/index.js
+++ src/index.js
@@ -26,7 +26,13 @@ class Board extends React.Component {
 
     handleClick(i) {
         const squares = this.state.squares.slice();
+
+        if (calculateWinner(squares) || squares[i]) {
+            return;
+        }
+
         squares[i] = this.state.xIsNext ? 'X' : 'O';
+        
         this.setState({
             squares: squares,
             xIsNext: !this.state.xIsNext

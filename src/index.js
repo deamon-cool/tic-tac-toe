cc72ec37dbc11765b3e04826f3f022a191dcdd3d
daemon-cool <damian.damian.tofik@gmail.com>
2020-06-26 10:07:14 +0200

    refactored

--- src/index.js
+++ src/index.js
@@ -4,15 +4,13 @@ import './index.css';
 
 let root = document.querySelector('#root');
 
-class Square extends React.Component {
-    render() {
-        return (
-            <button className='square'
-                onClick={() => { this.props.onClick() }}>
-                {this.props.value}
-            </button>
-        );
-    }
+function Square(props) {
+    return (
+        <button className='square'
+            onClick={() => { props.onClick() }}>
+            {props.value}
+        </button>
+    );
 }
 
 class Board extends React.Component {
@@ -81,4 +79,4 @@ class Game extends React.Component {
     }
 }
 
-ReactDOM.render(<Game/>, root);
+ReactDOM.render(<Game />, root);
